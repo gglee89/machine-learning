07801cf75009a54081be66cdfa1a80bcd61b413c
gglee89 <gglee89@gmail.com>
2019-04-01 18:29:27 -0500

    Find max_min with divide_and_conque algorithm

--- HW5-Algorithm/find_max_min.py
+++ HW5-Algorithm/find_max_min.py
@@ -19,19 +19,26 @@ def Merge(B, C, A):
         A[k:p+q] = B[i:p]
 
 
-def FindMaxMin(numbers):
+def Sort(numbers):
     lower_half = []
     upper_half = []
     if len(numbers) > 1:
         size = len(numbers)
         lower_half = numbers[0:size/2]
         upper_half = numbers[size/2:size]
-        FindMaxMin(lower_half)
-        FindMaxMin(upper_half)
+        Sort(lower_half)
+        Sort(upper_half)
         Merge(lower_half, upper_half, numbers)
         return numbers
 
 
-test = [4, 6, 2, 3, 4, 1, 8, 6, 9, 20]
-print(test)
-print(FindMaxMin(test))
+numbers = [4, 6, 2, 3, 4, 1, 8, 6, 9, 20]
+print('Numbers: [' + ','.join([str(number) for number in numbers]) + ']')
+
+numbers_sorted = Sort(numbers)
+print('Numbers Sorted: [' + ','.join([str(number)
+                                      for number in numbers_sorted]) + ']')
+
+minimum, maximum = numbers_sorted[0], numbers_sorted[len(numbers_sorted) - 1]
+print('Minimum: ' + str(minimum))
+print('Maximum: ' + str(maximum))
